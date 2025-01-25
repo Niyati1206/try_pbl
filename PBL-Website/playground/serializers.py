4079f41663f29a032b12c0ef41e2e7c2e242dcf2
Niyati1206 <115885145+Niyati1206@users.noreply.github.com>
2025-01-25 18:39:59 +0530

    Delete PBL-Website directory

--- PBL-Website/playground/serializers.py
+++
@@ -1,4 +0,0 @@
-from rest_framework import serializers
-
-class PDFUploadSerializer(serializers.Serializer):
-    file = serializers.FileField()  # Ensure this matches the 'file' parameter in the view
